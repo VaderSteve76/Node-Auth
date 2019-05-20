e38f2ed125298d53c6c860c78dcef1fb2b4474bd
Steve Alverson <vadersteve76@Steves-Mac-mini.cfl.rr.com>
2019-05-20 18:57:56 -0400

    Hashed password

--- routes/auth.js
+++ routes/auth.js
@@ -1,32 +1,50 @@
 const router = require('express').Router();
 const User = require('../model/User');
+const bcrypt = require('bcryptjs');
+const { registerValidation, loginValidation } = require('../validation');
 
-//Validation
-const Joi = require('@hapi/joi');
-
-const schema = {
-  name: Joi.string().min(6).required(),
-  email: Joi.string().min(6).required().email(),
-  password: Joi.string().min(6).required()
-}
 
 router.post('/register', async (req, res) => {
+//Validate data before becoming a user
+  const { error } = registerValidation(req.body);
+  if(error) return res.status(400).send(error.details[0].message);
 
-  //Validate data before becoming user
-  const {error} = Joi.validate(req.body, schema);
-  res.send(error.details[0].message);
+  //Check if user is already in DB
+  const emailExist = await User.findOne({ email: req.body.email });
+  if(emailExist) return res.status(400).send('email already exists');
 
-  // const user = new User({
-  //   name: req.body.name,
-  //   email: req.body.email,
-  //   password: req.body.password
-  // });
-  // try {
-  //   const savedUser = await user.save();
-  //   res.send(savedUser);
-  // } catch (err){
-  //   res.status(400).send(err);
-  // }
+  //Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(req.body.password, salt); 
+
+  //Create new user
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: hashPassword
+  });
+  try {
+    const savedUser = await user.save();
+    res.send({ user: user._id });
+  } catch (err){
+    res.status(400).send(err);
+  }
+});
+
+router.post('/login', async (req, res) => {
+  //Validate data before becoming a user
+  const { error } = loginValidation(req.body);
+  if(error) return res.status(400).send(error.details[0].message);
+
+  //Check if user is already in DB
+  const user = await User.findOne({ email: req.body.email });
+  if(!user) return res.status(400).send('Incorrect email or password');
+
+  //Password is correct
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if(!validPassword) return res.status(400).send('Invalid password');
+
+  res.send('Login Successful!');
 });
 
 
